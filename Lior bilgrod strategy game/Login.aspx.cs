3c8e64a2e73cb5b05d72d4d503c99ad1bb460fe7
liorbel <s41546@haklaiph.org>
2026-04-29 12:27:58 +0300

    after fixing 29/04/2026

--- Lior bilgrod strategy game/Login.aspx.cs
+++ Lior bilgrod strategy game/Login.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,18 +12,49 @@ public partial class Login : System.Web.UI.Page
     {
         if (IsPostBack)
         {
-            if ((Request.Form["userName"] == "test" && Request.Form["password"] == "1234")||(Request.Form["userName"] == "avi" && Request.Form["password"] == "4321"))
+            string userName = Request.Form["userName"];
+            string password = Request.Form["password"];
+
+            int userType = GetUserTypeFromDB(userName, password);
+
+            if (userType > 0)
             {
-                Session["userName"] = Request.Form["userName"];
+                Session["userName"] = userName;
                 Session["isLoggedIn"] = true;
 
                 Response.Redirect("Default.aspx");
             }
             else
             {
-                LoginResult.InnerText = "username or password are incorrect";
+                LoginResult.InnerText = "Password or username incorrect";
             }
         }
-
     }
+
+
+
+    //returns:
+    //0 - if user is not valid
+    //1 - is user is valid
+    private int GetUserTypeFromDB(string userName, string password)
+    {
+        string dbPath = this.MapPath("App_Data/Database.mdf");
+        DAL dal = new DAL(dbPath);
+
+        string sqlQuery = "SELECT * FROM Users " +
+                            "WHERE user_name = '" + userName +
+                            "' AND pswd = '" + password + "'";
+
+        DataTable dt = dal.GetDataTable(sqlQuery);
+
+        if (dt.Rows.Count == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
 }
