6f40b3d4e0da8aedabe8b42f54c357965be298cb
lior bel <liorbelgo@gmail.com>
2026-05-14 14:46:55 +0300

    newer the thing I said before this one

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Comments (
+    CommentID INT IDENTITY(1,1) PRIMARY KEY,
+    Username VARCHAR(255),
+    PageName VARCHAR(255),
+    CommentText NVARCHAR(MAX),
+    CommentDate DATETIME
+);
